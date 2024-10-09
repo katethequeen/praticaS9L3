c3e51ccfc03bcd139f72a6b6adcf217d2ee6246b
katethequeen <cateresa4@gmail.com>
2024-10-09 16:45:12 +0200

    First part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivePostsComponent } from './components/active-posts/active-posts.component';
+import { InactivePostsComponent } from './components/inactive-posts/inactive-posts.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivePostsComponent,
+    InactivePostsComponent,
+    PostDetailComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
