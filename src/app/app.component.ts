59a176dd448ff3b81848209282c1487b7b25891e
katethequeen <cateresa4@gmail.com>
2024-10-10 14:42:27 +0200

    Reviewing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
-import { Post } from './post';
-import jsonFile from '../../public/db.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'praticaS9L3';
-  posts: Post[] = []
-
-  posts:{id: number, title:string, body: string, userId: number, tags: string[], active: boolean}= jsonFile
-  }
-}
+export class AppComponent {}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { iPost } from '../../Models/post';
+import { iJSONresponse } from './../../Models/jsonresponse';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  postInEvidenza!: iPost;
+  arrPosts: iPost[] = [];
 
+  ngOnInit(): void {
+    fetch('db.json')
+      .then((res) => <Promise<iJSONresponse>>res.json())
+      .then((res) => {
+        console.log(res);
+
+        this.postInEvidenza = res.posts[0];
+        this.arrPosts = res.posts.slice(0, 4);
+      })
+      .catch((error) => console.log(error));
+  }
 }
